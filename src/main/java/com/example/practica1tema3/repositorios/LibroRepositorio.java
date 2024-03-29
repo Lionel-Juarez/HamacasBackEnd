79c107bf0ffc9eaf61ced80a857cce3c209c0754
Lionel-Juarez <gitlionel@gmail.com>
2024-03-29 20:48:10 +0100

    initial commit

--- src/main/java/com/example/practica1tema3/repositorios/LibroRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.example.practica1tema3.repositorios;
-
-import com.example.practica1tema3.entidades.Libro;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LibroRepositorio extends CrudRepository<Libro,Long> {
-}
