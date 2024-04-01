d374b3c97e34a99a4331e063a467d9bd00b5bc67
Lionel-Juarez <gitlionel@gmail.com>
2024-04-02 00:46:37 +0200

    backend report done

---
+++ src/main/java/com/example/hamacasbackend/HamacasBackEndApplication.java
@@ -0,0 +1,22 @@
+package com.example.hamacasbackend;
+
+
+import com.example.hamacasbackend.repositorios.ReportRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+@SpringBootApplication
+public class HamacasBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamacasBackEndApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(ReportRepository reportRepository) {
+        return (args) -> {
+        };
+    }
+
+}
--- src/test/java/com/example/hamacasbackend/HamacasBackEndApplicationTests.java
+++ src/test/java/com/example/hamacasbackend/HamacasBackEndApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Practica1Tema3ApplicationTests {
+class HamacasBackEndApplicationTests {
 
     @Test
     void contextLoads() {
