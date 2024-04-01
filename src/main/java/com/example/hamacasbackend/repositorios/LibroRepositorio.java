24502915dccd654e618b98e4ab632cf101e81bef
Lionel-Juarez <gitlionel@gmail.com>
2024-04-02 00:50:59 +0200

    backend report done

--- src/main/java/com/example/hamacasbackend/repositorios/LibroRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.example.hamacasbackend.repositorios;
-
-import com.example.hamacasbackend.entidades.Libro;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LibroRepositorio extends CrudRepository<Libro,Long> {
-}
